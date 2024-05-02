75b8ea7a651dcfb2f585191dd72c4d659ed6a10a
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-02 15:24:06 +0200

    add

---
+++ movie.php
@@ -0,0 +1,25 @@
+<?php 
+
+class Movie{
+  public $name;
+  public $actors;
+  public $year;
+
+  public function __construct(string $name, string $actors, string $year){
+    $this-> name = $_name;
+    $this-> actors = $actors;
+    $this-> year = $year;
+
+  }
+  {
+    
+  }
+
+
+  
+
+}
+
+
+
+?>
